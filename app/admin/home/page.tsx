ed922cc2dd169adbe03040848265731ef439f315
sudhir-sars <sudhir.sars@gmail.com>
2024-08-26 11:08:56 -0700

    added Home Button in nav of admin panel

--- app/admin/home/page.tsx
+++ app/admin/home/page.tsx
@@ -155,6 +155,13 @@ const HomePage = () => {
             </Link>
             <nav className="ml-auto flex items-center gap-4 sm:gap-6">
               <Link
+                href="/"
+                className="text-sm font-medium hover:underline underline-offset-4"
+                prefetch={false}
+              >
+                Home
+              </Link>
+              <Link
                 href="#"
                 className="text-sm font-medium hover:underline underline-offset-4"
                 prefetch={false}
